d95b2a90dca1c63d004b3fe32e3666b40ebc0b81
Mariusz Gumienny <43437107+0x8b@users.noreply.github.com>
2019-04-11 12:58:59 +0200

    -

---
+++ codeforces/0001B-Spreadsheet.py
@@ -0,0 +1,44 @@
+#! python
+
+import re
+import string
+
+upper = string.ascii_uppercase
+
+def dec2az(n):
+    out = ''
+
+    while n:
+        mod = (n - 1) % 26
+        n = (n - mod) // 26
+        out += upper[mod]
+
+    return out[::-1]
+
+def az2dec(s):
+    out = 0
+
+    for p, c in enumerate(s[::-1]):
+        out += 26 ** p * (ord(c) - ord('A') + 1)
+
+    return out
+
+rc  = re.compile(r"R(\d+)C(\d+)")
+cr  = re.compile(r"([A-Z]+)(\d+)")
+
+n = int(input())
+
+for _ in range(n):
+    cell = input()
+
+    if rc.match(cell):
+        m = rc.search(cell)
+        r = int(m.group(1))
+        c = dec2az(int(m.group(2)))
+        print(f"{c}{r}")
+
+    else:
+        m = cr.search(cell)
+        c = az2dec(m.group(1))
+        r = int(m.group(2))
+        print(f"R{r}C{c}")
---
+++ codeforces/0002A-Winner.py
@@ -0,0 +1,33 @@
+#! python
+
+from collections import defaultdict
+
+n = int(input())
+m = float('-inf')
+winner = None
+data = []
+results = defaultdict(int)
+
+for _ in range(n):
+    name, score = input().split()
+    score = int(score)
+
+    results[name] += score
+
+    data.append((name, score))
+
+m = max(results.values())
+
+names = [name for name, score in results.items() if score == m]
+
+results = defaultdict(int)
+
+for name, score in data:
+    if name in names:
+        results[name] += score
+
+        if results[name] >= m:
+            winner = name
+            break
+
+print(winner)
